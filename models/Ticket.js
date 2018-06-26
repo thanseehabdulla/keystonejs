2c5b111b955903401f791fca4a5874464361e58d
Abdulla thanseeh <thanseehabdulla@gmail.com>
2018-06-26 11:38:27 +0530

    project added

--- models/Ticket.js
+++ models/Ticket.js
@@ -2,16 +2,15 @@ var keystone = require('keystone');
 var Types = keystone.Field.Types; ;
 
 var Ticket = new keystone.List('Ticket', {
-	autokey: { from: 'title', path: 'slug', unique: true },
-	searchFields: 'description',
+     	autokey: { from: 'title', path: 'slug', unique: true },
 });
 
 Ticket.add({
-	title: { type: Types.Text, default: '', required: true },
+	title: { type: String, initial: true, default: '', required: true },
 	description: { type: Types.Textarea },
-	priority: { type: Types.Select, options: 'Low, Medium, High', default: ' Low' },
+	priority: { type: Types.Select, options: 'Low, Medium, High', default: 'Low' },
 	category: { type: Types.Select, options: 'Bug, Feature, Enhancement', default: 'Bug' },
-	status: { type: Types.Select, options: 'New, In Progress, Open, On Hold, Declined, Closed', default: 'New' },
+	status: { type: Types.Select, options: 'New, In Progress, Open, On Hold,Declined, Closed', default: 'New' },
 	createdBy: { type: Types.Relationship, ref: 'User', index: true, many: false },
 	assignedTo: { type: Types.Relationship, ref: 'User', index: true, many: false },
 	createdAt: { type: Types.Datetime, default: Date.now },
@@ -19,5 +18,5 @@ Ticket.add({
 });
 
 Ticket.defaultSort = '-createdAt';
-Ticket.defaultColumns = 'title|20%, status|15%, createdBy, assignedTo, createdAt';
+Ticket.defaultColumns = 'title|20%, status|15%, createdBy, assignedTo, createdAt ';
 Ticket.register();
